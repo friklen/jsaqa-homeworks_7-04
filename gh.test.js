491cc344d3eccaad7149391a46a5d57a76b52a4b
friklen <friklen@gmail.com>
2025-04-19 21:17:50 +0400

    puppeteer commit

---
+++ gh.test.js
@@ -0,0 +1,70 @@
+let page;
+
+// beforeEach(async () => {
+//   page = await browser.newPage();
+//   await page.goto("https://github.com/team");
+// });
+
+afterEach(() => {
+  page.close();
+});
+
+describe("Github page tests", () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/team");
+  });
+
+  test("The h1 header content'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector("h1");
+    const title2 = await page.title();
+    expect(title2).toEqual(
+      "GitHub for teams · Build like the best teams on the planet · GitHub"
+    );
+  }, 3000);
+
+  test("The first link attribute", async () => {
+    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
+    expect(actual).toEqual("#start-of-content");
+  }, 3000);
+
+  test("The page contains Sign in button", async () => {
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
+    expect(actual).toContain("Get started with Team");
+  }, 3000);
+});
+
+describe("Github page tests. Pricing", () => {
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/pricing");
+  });
+
+  test("The h1 header content. Pricing'", async () => {
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector("h1");
+    const title2 = await page.title();
+    expect(title2).toEqual("Pricing · Plans for every developer · GitHub");
+  }, 6000);
+
+  test("The first link attribute. Pricing", async () => {
+    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
+    expect(actual).toEqual("#start-of-content");
+  }, 6000);
+
+  test("The page contains button. Pricing", async () => {
+    const btnSelector = ".btn-mktg.d-block.js-pricing-upgrade-path";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
+    expect(actual).toContain("Continue with Team");
+  }, 6000);
+});
